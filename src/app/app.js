e840bd3a68eaaf64329bb36070547eb31208895c
IMATMI-gehan <gehan@imatmi.com>
2021-09-15 05:36:53 +0530

    MD-1 Initial Setup

---
+++ src/app/app.js
@@ -0,0 +1,61 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useRef } from 'react';
+import { Provider } from 'react-redux';
+import { Switch } from 'react-router-dom';
+import store from '../store';
+import './app.scss';
+import { loadUser } from './core/actions/authentication';
+import AuthGuard from './core/guards/authentication.guard';
+import { setAuthToken } from './core/services/central-operations.service';
+import Layout from './layout';
+import Dashboard from './modules/dashboard/dashboard';
+import GettingStarted from './modules/getting-started/getting-started';
+import Resume from './modules/resume/resume';
+import { Pages } from './shared/constants/routes';
+require('./core/interceptors');
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = (props) => {
+  const toast = useRef(null);
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []); //read react.js documentation for explanation
+
+  return (
+    <Provider store={store}>
+      <Layout>
+        <section className='container-fluid p-0'>
+          <div className='wrapper'>
+            {/* <Toast ref={toast} />
+            <Button
+              label='Success'
+              className='p-button-success'
+              onClick={showMessage(
+                toast,
+                AlertSeverity.Error,
+                'Sumaary',
+                'details'
+              )}
+            /> */}
+            <Switch>
+              <AuthGuard
+                exact
+                path={Pages.dashboard.link}
+                component={Dashboard}
+              />
+              <AuthGuard
+                exact
+                path={Pages.gettingStarted.link}
+                component={GettingStarted}
+              />
+            </Switch>
+          </div>
+        </section>
+      </Layout>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/app/shared/constants/routes.js
@@ -0,0 +1,55 @@
+export const Pages = {
+  landingPage: {
+    id: 1,
+    link: '/',
+    text: 'Landing Page'
+  },
+  login: {
+    id: 2,
+    link: '/login',
+    text: 'Login'
+  },
+  register: {
+    id: 3,
+    link: '/register',
+    text: 'Register'
+  },
+  authenticate: {
+    id: 4,
+    link: '/authenticate',
+    text: 'Verify user'
+  },
+  dashboard: {
+    id: 5,
+    link: '/dashboard',
+    text: 'dashboard'
+  },
+  add_question: {
+    id: 5,
+    link: '/add_question',
+    text: 'add_question'
+  },
+  profile: {
+    id: 5,
+    link: '/profile',
+    text: 'Profile'
+  },
+  gettingStarted: {
+    id: 6,
+    link: '/getting-started',
+    text: 'getting-started'
+  }
+};
+
+export const apiVersion = {
+  v1: 'V1.0'
+};
+
+export const APIRoutes = {
+  task: {
+    url: 'task'
+  },
+  taskByDate: {
+    url: 'taskByDate'
+  }
+};
