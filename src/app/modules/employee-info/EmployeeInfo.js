5c5afb4bfd7e61bfedafd61e4b5b944d93ab2d72
Dark Lord <bhosalepranil16@gmail.com>
2022-03-20 11:04:07 +0530

    ui changes

--- src/app/modules/employee-info/EmployeeInfo.js
+++ src/app/modules/employee-info/EmployeeInfo.js
@@ -1,11 +1,25 @@
 import react, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { OrderList } from 'primereact/orderlist';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Ripple } from 'primereact/ripple';
+import { classNames } from 'primereact/utils';
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
+import { InputText } from 'primereact/inputtext';
 
 import './employeeInfo.css';
 
 const EmployeeInfo = (props) => {
   const [AllUsers, setAllUsers] = useState([]);
+  const [first, setFirst] = useState(0);
+  const [rows, setRows] = useState(10);
+  const [filters1, setFilters1] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    name: {
+      operator: FilterOperator.AND,
+      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }]
+    }
+  });
 
   useEffect(() => {
     getAllMembers();
@@ -13,39 +27,162 @@ const EmployeeInfo = (props) => {
 
   const getAllMembers = async () => {
     const res = await axios.get(`getAllUsers`);
-    console.log('all members', res?.data);
     setAllUsers(res?.data);
   };
 
-  const itemTemplate = (item) => {
+  const paginatorTemplate = {
+    layout: 'PrevPageLink PageLinks NextPageLink CurrentPageReport',
+    PrevPageLink: (options) => {
+      return (
+        <button
+          type='button'
+          className={options.className}
+          onClick={options.onClick}
+          disabled={options.disabled}
+        >
+          <span className='p-3'>Previous</span>
+          <Ripple />
+        </button>
+      );
+    },
+    NextPageLink: (options) => {
+      return (
+        <button
+          type='button'
+          className={options.className}
+          onClick={options.onClick}
+          disabled={options.disabled}
+        >
+          <span className='p-3'>Next</span>
+          <Ripple />
+        </button>
+      );
+    },
+    PageLinks: (options) => {
+      if (
+        (options.view.startPage === options.page &&
+          options.view.startPage !== 0) ||
+        (options.view.endPage === options.page &&
+          options.page + 1 !== options.totalPages)
+      ) {
+        const className = classNames(options.className, { 'p-disabled': true });
+
+        return (
+          <span className={className} style={{ userSelect: 'none' }}>
+            ...
+          </span>
+        );
+      }
+
+      return (
+        <button
+          type='button'
+          className={options.className}
+          onClick={options.onClick}
+        >
+          {options.page + 1}
+          <Ripple />
+        </button>
+      );
+    },
+    CurrentPageReport: (options) => {
+      return (
+        <span
+          className='mx-3'
+          style={{ color: '##5763a6', userSelect: 'none' }}
+        ></span>
+      );
+    }
+  };
+
+  const onCustomPage = (event) => {
+    setFirst(event.first);
+    setRows(event.rows);
+  };
+
+  const renderHeader = (filtersKey) => {
+    const filters = filtersMap[`${filtersKey}`].value;
+
     return (
-      <>
-        <div className='row product-item'>
-          <div className='col-md-6'>
-            <p>{item.name}</p>
-          </div>
-          <div className='col-md-6'>
-            <p>{item.email}</p>
-          </div>
-        </div>
-      </>
+      <span className='p-input-icon-left'>
+        <i className='pi pi-search' />
+        <InputText
+          type='search'
+          value={filters['global'].value || ''}
+          onChange={(e) => onGlobalFilterChange(e, filtersKey)}
+          placeholder='Employee Search'
+        />
+      </span>
     );
   };
 
+  const onGlobalFilterChange = (event, filtersKey) => {
+    const value = event.target.value;
+    let filters = { ...filtersMap[filtersKey].value };
+    filters['global'].value = value;
+
+    filtersMap[filtersKey].callback(filters);
+  };
+
+  const filtersMap = {
+    filters1: { value: filters1, callback: setFilters1 }
+  };
+
+  const header1 = renderHeader('filters1');
+
   return (
-    <div className='container'>
-      <div className='orderlist-demo'>
-        <div className='card'>
-          <OrderList
-            value={AllUsers}
-            header='Employees'
-            listStyle={{ height: '100%' }}
-            dataKey='_id'
-            itemTemplate={itemTemplate}
-          ></OrderList>
+    <>
+      <div className='container' style={{ background: '##f5f6fb' }}>
+        <div className='row'>
+          <div className='col-md-12 h4 font-weight-bold'>
+            Employee Information
+          </div>
         </div>
+        <DataTable
+          value={AllUsers}
+          paginator
+          paginatorTemplate={paginatorTemplate}
+          first={first}
+          rows={rows}
+          onPage={onCustomPage}
+          header={header1}
+          filters={filters1}
+          onFilter={(e) => setFilters1(e.filters)}
+        >
+          <Column
+            headerStyle={{ color: '#5763a6' }}
+            field='name'
+            header='Name'
+            sortable
+          ></Column>
+          <Column
+            headerStyle={{ color: '#5763a6' }}
+            field='designation'
+            header='Designation'
+          ></Column>
+          <Column
+            headerStyle={{ color: '#5763a6' }}
+            field='email'
+            header='Email'
+          ></Column>
+          {/* <Column
+            headerStyle={{ color: '#5763a6' }}
+            field='mobile'
+            header='Mobile'
+          ></Column> */}
+          {/* <Column
+            headerStyle={{ color: '#5763a6' }}
+            field='gender'
+            header='Gender'
+          ></Column>
+          <Column
+            headerStyle={{ color: '#5763a6' }}
+            field='team'
+            header='Team'
+          ></Column> */}
+        </DataTable>
       </div>
-    </div>
+    </>
   );
 };
 
